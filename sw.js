64e82d0b6bfe46a5d8b46231f1155b9e2773a653
rogeliovalle <rogelio.valle07@gmail.com>
2026-03-16 16:28:26 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,14 @@
-const CACHE_NAME = 'autolog-v12-offline';
+const CACHE_NAME = 'autolog-barcode-v13';
 const assets = [
   './',
   './index.html',
-  'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js'
+  'https://unpkg.com/html5-qrcode'
 ];
 
-// Este evento ocurre la primera vez que abren la app con internet
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log('Caché guardada: Ahora la app puede trabajar sin señal.');
-      return cache.addAll(assets);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-// Este evento es el que permite que la app funcione aunque no haya señal
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      // Si el archivo está en la memoria, lo usa. Si no, intenta buscarlo en internet.
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
