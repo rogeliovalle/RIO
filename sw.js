2324598093efe547d4775598bc848d43d31091b3
rogeliovalle <rogelio.valle07@gmail.com>
2026-03-16 16:43:56 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,15 @@
-const CACHE_NAME = 'autolog-barcode-v13';
+const CACHE_NAME = 'autolog-v13.1-barcode';
 const assets = [
   './',
   './index.html',
-  'https://unpkg.com/html5-qrcode'
+  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+  self.skipWaiting(); // Fuerza a la nueva versión a activarse
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
 });
 
 self.addEventListener('fetch', e => {
